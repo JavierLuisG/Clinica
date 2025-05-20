16cb33b227d8cc147ad1f35cf367ffeb3396b0b1
Javier Andrés Luis González <javierandresluisgonzalez@gmail.com>
2025-05-19 23:54:22 -0500

    feat: add database configuration with H2

--- src/main/java/com/backend/clinica/ClinicaOdontologicaApplication.java
+++ src/main/java/com/backend/clinica/ClinicaOdontologicaApplication.java
@@ -1,13 +1,14 @@
 package com.backend.clinica;
 
+import com.backend.clinica.db.H2Connection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClinicaOdontologicaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+    H2Connection.createTable();
+  }
 }
---
+++ src/main/java/com/backend/clinica/db/H2Connection.java
@@ -0,0 +1,40 @@
+package com.backend.clinica.db;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class H2Connection {
+  private static final Logger LOGGER = LoggerFactory.getLogger(H2Connection.class);
+  public static Connection getConnection() {
+    Connection conn = null;
+    try {
+      Class.forName("org.h2.Driver");
+      conn = DriverManager.getConnection("jdbc:h2:~/clinica", "sa", "sa");
+    } catch (ClassNotFoundException | SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return conn;
+  }
+
+  public static void createTable() {
+    Connection conn = null;
+    try {
+      Class.forName("org.h2.Driver");
+      conn = DriverManager.getConnection("jdbc:h2:~/clinica;INIT=RUNSCRIPT FROM 'create.sql'", "sa", "sa");
+    } catch (ClassNotFoundException | SQLException e) {
+      LOGGER.error("Error al inicializar la base de datos: " + e);
+    } finally {
+      if (conn != null) {
+        try {
+          conn.close();
+        } catch (SQLException e) {
+          LOGGER.error("Error al cerrar la conexión: " + e);
+        }
+      }
+    }
+  }
+}
